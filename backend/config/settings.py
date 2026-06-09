77a6646c09dc4b3189a4b924fa90023a31a81908
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-06-10 01:05:43 +0530

    Implement Docker Compose multi-service architecture

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -174,6 +174,6 @@ LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
